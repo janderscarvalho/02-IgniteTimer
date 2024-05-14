db28b685a77eaa88b87eccdc6a1ccff2d71a748e
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2024-05-14 18:22:25 -0300

    Salvando no Storage
    Foi realizado a função de Salvamento dos nosso ciclos em nosso Storage, para manter registros de nossos ciclos

--- src/pages/Home/Components/Countdown/index.tsx
+++ src/pages/Home/Components/Countdown/index.tsx
@@ -21,7 +21,7 @@ export function CountDown() {
       interval = setInterval(() => {
         const secondsDiference = differenceInSeconds(
           new Date(),
-          activeCycle.startDate,
+          new Date(activeCycle.startDate),
         )
 
         if (secondsDiference >= totalSeconds) {
